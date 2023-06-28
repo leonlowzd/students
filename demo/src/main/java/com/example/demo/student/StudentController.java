9e507f71f2ab3c5403d5566db2b2f49ecb6556ff
Leon Low <leonlow2010@gmail.com>
2023-06-28 15:12:50 +0800

    Added Delete and Put function

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -21,4 +21,17 @@ public class StudentController {
     public void registerNewStudent(@RequestBody Student student){
         studentService.addNewStudent(student);
     }
+
+    @DeleteMapping(path = "{studentID}")
+    public void deleteStudent(@PathVariable("studentID") Long id) {
+        studentService.deleteStudent(id);
+    }
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
+    }
+
 }
